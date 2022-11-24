13f1e300d4648bce2b0ffbaa6dae85542aa50585
sivaramkanikanti <sivaramkanikanti@sivas-mbp>
2022-11-24 15:18:14 -0500

    demo2 project added.

---
+++ demo2/src/com/demo1/main/main.java
@@ -0,0 +1,11 @@
+package com.demo1.main;
+
+public class main {
+
+	public static void main(String[] args) 
+	{
+		//TODO Auto-generated method stub
+        System.out.println("Welcome to barcelona");
+	}
+
+}

274971ae0ba18f489aa15757890402f98faa2e35
sivaramkanikanti <sivaramkanikanti@sivas-mbp>
2024-07-07 23:08:15 -0400

    efef

--- demo2/src/com/demo1/main/main.java
+++ demo2/src/com/demo1/main/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) 
 	{
 		//TODO Auto-generated method stub
-        System.out.println("Welcome to barcelona Messi 123 ");
+        System.out.println("Welcome to barcelona Messi 101 ");
 	}
 
 }
